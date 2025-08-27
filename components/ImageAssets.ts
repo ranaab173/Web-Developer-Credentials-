cbba57d4b2c334f37cad490270ddd911c072874c
ranaab173 <ranairc@gmail.com>
2025-08-28 00:10:22 +0500

    new data

--- components/ImageAssets.ts
+++ components/ImageAssets.ts
@@ -10,7 +10,7 @@
 export const profilePic = 'https://images.unsplash.com/photo-1755719985500-b6049b2ecb1f?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 
 // About Section
-export const aboutMeImage = './images/about-me.jpg';
+export const aboutMeImage = 'images/about-me.jpg';
 
 // Portfolio Section
 export const portfolioImages = {
