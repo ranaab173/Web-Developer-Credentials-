27d89a63fba189b785d48d5769f981209e061f04
Abubakar Siddiq <ranairc@gmail.com>
2025-08-28 16:46:34 +0500

    feat: Update project name and image asset paths
    Renames the project to "abubakarwebteck" and updates image asset URLs to reflect the new repository name. This ensures all references are consistent with the project's identity.

--- components/ImageAssets.ts
+++ components/ImageAssets.ts
@@ -1,15 +1,15 @@
 export const ImageAssets = {
-  profilePic: 'https://raw.githubusercontent.com/ranaab173/Web-Developer-Credentials-/main/images/profilepic.jpg',
-  aboutMe: 'https://raw.githubusercontent.com/ranaab173/Web-Developer-Credentials-/main/images/about-me.jpg',
+  profilePic: 'https://raw.githubusercontent.com/ranaab173/abubakarwebteck/main/images/profilepic.jpg',
+  aboutMe: 'https://raw.githubusercontent.com/ranaab173/abubakarwebteck/main/images/about-me.jpg',
   portfolio: {
-    card1: 'https://raw.githubusercontent.com/ranaab173/Web-Developer-Credentials-/main/images/card1.jpg',
-    card2: 'https://raw.githubusercontent.com/ranaab173/Web-Developer-Credentials-/main/images/card2.jpg',
-    card3: 'https://raw.githubusercontent.com/ranaab173/Web-Developer-Credentials-/main/images/card3.jpg',
-    card4: 'https://raw.githubusercontent.com/ranaab173/Web-Developer-Credentials-/main/images/card4.jpg',
+    card1: 'https://raw.githubusercontent.com/ranaab173/abubakarwebteck/main/images/card1.jpg',
+    card2: 'https://raw.githubusercontent.com/ranaab173/abubakarwebteck/main/images/card2.jpg',
+    card3: 'https://raw.githubusercontent.com/ranaab173/abubakarwebteck/main/images/card3.jpg',
+    card4: 'https://raw.githubusercontent.com/ranaab173/abubakarwebteck/main/images/card4.jpg',
   },
   testimonials: {
-    janeDoe: 'https://raw.githubusercontent.com/ranaab173/Web-Developer-Credentials-/main/images/jane-doe.jpg',
-    johnSmith: 'https://raw.githubusercontent.com/ranaab173/Web-Developer-Credentials-/main/images/john-smith.jpg',
-    emilyWhite: 'https://raw.githubusercontent.com/ranaab173/Web-Developer-Credentials-/main/images/emily-white.jpg',
+    janeDoe: 'https://raw.githubusercontent.com/ranaab173/abubakarwebteck/main/images/jane-doe.jpg',
+    johnSmith: 'https://raw.githubusercontent.com/ranaab173/abubakarwebteck/main/images/john-smith.jpg',
+    emilyWhite: 'https://raw.githubusercontent.com/ranaab173/abubakarwebteck/main/images/emily-white.jpg',
   },
 };
