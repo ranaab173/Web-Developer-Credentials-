67bcc165987e30157a1fae08d44f0287b3a61d88
ranaab173 <ranairc@gmail.com>
2025-08-28 12:17:36 +0500

    new data

--- components/ImageAssets.ts
+++ components/ImageAssets.ts
@@ -1,15 +1,15 @@
 export const ImageAssets = {
-  profilePic: '/images/profilepic.jpg',
-  aboutMe: '/images/about-me.jpg',
+  profilePic: './images/profilepic.jpg',
+  aboutMe: './images/about-me.jpg',
   portfolio: {
-    card1: '/images/card1.jpg',
-    card2: '/images/card2.jpg',
-    card3: '/images/card3.jpg',
-    card4: '/images/card4.jpg',
+    card1: './images/card1.jpg',
+    card2: './images/card2.jpg',
+    card3: './images/card3.jpg',
+    card4: './images/card4.jpg',
   },
   testimonials: {
-    janeDoe: '/images/jane-doe.jpg',
-    johnSmith: '/images/john-smith.jpg',
-    emilyWhite: '/images/emily-white.jpg',
+    janeDoe: './images/jane-doe.jpg',
+    johnSmith: './images/john-smith.jpg',
+    emilyWhite: './images/emily-white.jpg',
   },
 };
